0ce82e60f2703d448a710150af67036327533109
AshMargaryan <margaryanashot279@gmail.com>
2024-03-17 19:39:54 +0400

    Implemented Authorizetion

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import PostSerializer
 from .models import Post
-
-
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostListView(ListAPIView):
@@ -12,10 +12,12 @@ class PostListView(ListAPIView):
 
 
 class PostDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class CreatePostView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
--- products_/views.py
+++ products_/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ProductSerializer
 from .models import Product
+from .permissions import IsAuthorOrReadOnly
 
 
 class ProductListView(ListAPIView):
@@ -10,10 +12,12 @@ class ProductListView(ListAPIView):
 
 
 class ProductDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class PostProductView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
